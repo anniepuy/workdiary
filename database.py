c3014cdbea9203f70cd640d707c92ca37e572d95
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-09-26 14:28:06 -0500

    updated database connection functions

--- database.py
+++ database.py
@@ -6,8 +6,13 @@ entries = []
 
 connection = sqlite3.connect("workdata.db")
 
+def create_table():
+    with connection:
+        connection.execute("CREATE TABLE IF NOT EXIST entries (content TEXT, date TEXT);")
+
 def add_entry(entry_content, entry_date):
-    entries.append({"content": entry_content, "date": entry_date})
+    with connection:
+        connection.execute("INSERT INTO entries VALUES ('Test Content', '09-09-2022');")
 
 def get_entries():
     return entries 
