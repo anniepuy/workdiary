addc1b1376e436e04ed0c173ee3d54031c1601e8
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-09-26 09:24:39 -0500

    added connection to sqlite

--- database.py
+++ database.py
@@ -1,4 +1,10 @@
+import sqlite3
+
+
+import sqlite3
 entries = []
+
+connection = sqlite3.connect("workdata.db")
 
 def add_entry(entry_content, entry_date):
     entries.append({"content": entry_content, "date": entry_date})
